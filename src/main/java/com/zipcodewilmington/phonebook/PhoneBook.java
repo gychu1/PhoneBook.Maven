e7a0a55bc994c052312e741d2bb1522c6f35824d
Git-Yee <git-yee@ZipcodersMBP58.fios-router.home>
2022-07-13 01:12:53 -0400

    finsihing lab

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -16,39 +13,83 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook = map;
     }
 
     public PhoneBook() {
-        this(null);
+        this.phonebook = new LinkedHashMap<>(); //this is initializing the variable called phonebook
+//        this(null);
     }
 
     public void add(String name, String phoneNumber) {
+        List<String> listOfPhoneNumbers = new ArrayList<>();
+        listOfPhoneNumbers.add(phoneNumber);
+        this.phonebook.put(name, listOfPhoneNumbers);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+//        List<String> listOfPhoneNumbers = new ArrayList<>();
+//        List<String> list = Arrays.asList(phoneNumbers);
+//        listOfPhoneNumbers.addAll(list);
+//        this.phonebook.put(name, listOfPhoneNumbers);
+
+        List<String> list = Arrays.asList(phoneNumbers);
+        List<String> listOfPhoneNumbers = new ArrayList<>(list);
+        this.phonebook.put(name, listOfPhoneNumbers);
     }
 
     public void remove(String name) {
+        this.phonebook.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        if (phonebook.containsKey(name)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Boolean hasEntry(String name, String phoneNumber) {
+        //the test is looking for if the String phoneNumber is in the value, which is a list of string.
+        //String is not the same as a List<String>, so we turn the String into a List<String.
+        if (phonebook.containsKey(name) && phonebook.containsValue(Collections.singletonList(phoneNumber))) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return phonebook.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
+        //Map.Entry<String, List<String>> -- we are calling the interface Map Entry. This is the TYPE.
+        //.entrySet() -- This is all the key and values.
+        //for each K-V pair (variable entry), in the list of K-V pairs.
+        //to the left of the colon, it is a temporary variable where we store the current iteration of K-V pair in phonebook.
+        //for each number in the list of String phoneNumbers
+        for (Map.Entry<String, List<String>> entry : phonebook.entrySet()) {
+            //for each key-value pair in phonebook, we are checking .getValue
+            for (String number : entry.getValue()) {
+                //in this case, .getValue is getting back a LIST of Strings. We are iterating thru the list of string
+                //to compare each element in that list of string to phoneNumber.
+                if (number.equals(phoneNumber)) {
+                    return entry.getKey();
+                }
+            }
+        }
         return null;
+        //there is another way that is more efficient that is thru "streams"
     }
 
     public List<String> getAllContactNames() {
-        return null;
+//        List<String> names = new ArrayList<>(this.phonebook.keySet());
+        return new LinkedList<>(this.phonebook.keySet());
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return phonebook;
     }
 }
